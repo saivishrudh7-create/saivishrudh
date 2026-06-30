200363a94eb8452645989cb91cb97ad681f4a146
saivishrudh7-create <saivishrudh7@gmail.com>
2026-06-30 18:05:53 +0530

    Update day 2 task.py

--- day 2 task.py
+++ day 2 task.py
@@ -1,34 +1,43 @@
 # 1)student pass or fail
-'''m=int(input("enter your mark:"))
+'''
+m=int(input("enter your mark:"))
 if m>=35:
     print("pass")
 else:
-    print("fail")'''
+    print("fail")
+'''
 
 #2)check voting eligibility
-'''a=int(input("enter your age:"))
+'''
+a=int(input("enter your age:"))
 if a>=18:
       print("you are eligible to vote")
 else:
-    print("not eligible")'''
+    print("not eligible")
+'''
 
 
 #3)calculate the customer final amount
-'''amount=5000
+'''
+amount=5000
 d=10
 final=amount*d/100
-print("final amount:",final)'''
+print("final amount:",final)
+'''
 
 #4)verify the password
-'''s_pass=1234
+'''
+s_pass=1234
 p=int(input("enter your password:"))
 if p==s_pass:
       print("you can access a website")
 else:
-    print("wrong password")'''
+    print("wrong password")
+'''
 
 #5)based on marks
-'''m=int(input("enter your marks:"))
+'''
+m=int(input("enter your marks:"))
 if m>=90:
       print("A grade")
 elif m>=75:
@@ -36,10 +45,12 @@ elif m>=75:
 elif m>=50 :
     print("C grade")
 else:
-    print("fail")'''
+    print("fail")
+'''
 
 #6)Simple calculator
-'''a=int(input("enter your a:"))
+'''
+a=int(input("enter your a:"))
 b=int(input("enter your b:"))
 add=a+b
 sub=a-b
@@ -48,10 +59,12 @@ div=a/b
 print("addition:",add)
 print("subtraction:",sub)
 print("multiplilcation:",mul)
-print("Division:",div)'''
+print("Division:",div)
+'''
 
 #7)Calculate electricity charges
-'''u=int(input("enter you units:"))
+'''
+u=int(input("enter you units:"))
 if u<=101:
       print("₹2/unit")
 elif u<=200:
@@ -59,10 +72,12 @@ elif u<=200:
 elif u>200:
     print("₹5/unit")
 else:
-    print("enter correct units:")'''
+    print("enter correct units:")
+'''
 
 #8)ticket fare based on age
-'''a=int(input("enter your age:"))
+'''
+a=int(input("enter your age:"))
 if a<5:
     print("Ticket free")
 elif a<12:
@@ -70,15 +85,18 @@ elif a<12:
 elif a>=12:
     print("Ticket price 100")
 else:
-    print("enter your age correct")'''
+    print("enter your age correct")
+'''
 
 #9) bank approves a loan
-'''a=int(input("enter you amount:"))
+'''
+a=int(input("enter you amount:"))
 c=int(input("enter you credit score:"))
 if a>=30000 and c>=700:
       print("Eligible for loan approved")
 else:
-    print(" Not Eligible")'''
+    print(" Not Eligible")
+'''
 
 #10)student is eligible for college admission
 a=int(input("enter your academics score:"))
@@ -88,23 +106,28 @@ if a>60 and a>70:
 else:
     print("not eligible")
     #11)A company recruits candidates check eligible or not
-'''d=int(input("enter your degree %:"))
+'''
+d=int(input("enter your degree %:"))
 a=int(input("enter your aptitude Score:"))
 if d>65 and a>75:
     print("eligible to attend next round")
 else:
-    print("not eligible")'''
+    print("not eligible")
+'''
 
 #12)A scholarship is awarded for student to check eligibe or not
-'''s=int(input("enter your score:"))
+'''
+s=int(input("enter your score:"))
 a=int(input("enter your attendance %"))
 if s>80 and a>75:
     print("eligible for scholarship")
 else:
-    print("not eligible")'''
+    print("not eligible")
+'''
 
 #13)check the user and pin atm efficient balance
-'''amount=1000
+'''
+amount=1000
 a=int(input("enter your amount:"))
 if a<=amount:
     pin=int(input("enter your pin:"))
@@ -113,20 +136,24 @@ if a<=amount:
     else:
         print("wrong pin")
 else:
-    print("insufficent balance")'''
+    print("insufficent balance")
+'''
 
 #14)verify the username and password
-'''user="jothi"
+'''
+user="jothi"
 password=4567
 u=str(input("enter your username:"))
 p=int(input("enter your password:"))
 if user==u and password==p:
     print("generating otp")
 else:
-    print("invalid username or password")'''
+    print("invalid username or password")
+'''
 
 #15)check the doctor is available
-'''doctor="gopi"
+'''
+doctor="gopi"
 d=str(input("enter doctor name:"))
 if doctor==d:
     s=int(input("enter the slots"))
@@ -135,10 +162,12 @@ if doctor==d:
     else:
         print("not")
 else:
-    print("doctor not available")'''
+    print("doctor not available")
+'''
 
 #16)checking student can access the cybersecurity lab
-'''r=int(input("enter the register no:"))
+'''
+r=int(input("enter the register no:"))
 if r>100:
     c=int(input("enter your course fees:"))
     if c==35000:
@@ -146,10 +175,12 @@ if r>100:
     else:
           print(" fees balance")
 else:
-    print("invalid reg")'''
+    print("invalid reg")
+'''
 
 #17)employee is eligible
-'''e=int(input("enter your experience:"))
+'''
+e=int(input("enter your experience:"))
 if e>3:
     r=int(input("enter your ratings:"))
     if r>8:
@@ -157,22 +188,27 @@ if e>3:
     else:
         print("rating less")
 else:
-    print("experience is less")'''
+    print("experience is less")
+'''
 
 #18)checking fees detail for an online examination portal 
-'''fees=90000
+'''
+fees=90000
 f=int(input("enter your fees:"))
 if fees==f:
     print("hall ticket is available")
 else:
-    print("fees not cleared")'''
+    print("fees not cleared")
+'''
 
 #19) A hostel room can be allotted only if the student has been selected for admission
-'''a=int(input("enter your admission no:"))
+'''
+a=int(input("enter your admission no:"))
 if a>100:
     print("you are alloted by hotel")
 else:
-    print("you not selected by admission")'''
+    print("you not selected by admission")
+'''
 
 #20)An online shopping website offers:
 amount=5000
