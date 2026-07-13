3628c694a65fe37b6d5aa50f088ea3dda46375a0
saivishrudh7-create <saivishrudh7@gmail.com>
2026-07-13 08:05:17 +0530

    Add files via upload

---
+++ day 4 and lab 2nd task.py
@@ -0,0 +1,60 @@
+'''
+for i in range(1,101):
+    print(i)
+    if i==50:
+        break
+'''
+'''
+correct_password="python123"
+
+while True:
+    password=input("Enter password:")
+
+    if password==correct_password:
+        print("Access Granted")
+        break
+    else:
+        print("Wrong password.Try again.")
+        '''
+'''
+names=["Arun","Bala","Kumar","Sai","Rahul"]
+search=input("Enter name to search:")
+
+for name in names:
+    if name==search:
+        print("Name found")
+        break
+    else:
+        print("Name not found")
+        '''
+'''
+for i in range(1,21):
+    if i%3==0:
+        continue
+    print(i)'''
+'''
+for i in range(1,51):
+    if i%2==0:
+        continue
+    print(i)
+    '''
+'''
+text=input("Enter a string:")
+
+for ch in text:
+    if ch.lower()in "aeiou":
+        continue
+    print(ch,end=")'''
+'''
+def my_function():
+    pass
+'''
+'''
+class myclz:
+    pass'''
+'''
+for i in range(1,6):
+    if i==3:
+        pass
+    else:
+        print(i)'''
